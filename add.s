ecf3b03b9e6bd80c07befe79687972f8739cfbbe
Enio Fernandes <ef@cic.ac>
2016-12-21 01:11:43 +0000

    AQA ARM

--- add.s
+++ add.s
@@ -1,8 +1,8 @@
-mov     r0, #5          @ Load register r0 with the value 5
-mov     r1, #4          @ Load register r1 with the value 4
-add     r2, r1, r0      @ Add r0 and r1 and store in r2
+MOV R0, #5          @ Load register R0 with the value 5
+MOV R1, #4          @ Load register R1 with the value 4
+ADD R2, R1, R0      @ Add R0 and R1 and store in R2
 
-stop:   b stop          @ Infinite loop to stop execution
+HALT:   B HALT      @ Infinite loop to stop execution
  
 /*
 R00=00000005 R01=00000004 R02=00000009 R15=10010040
